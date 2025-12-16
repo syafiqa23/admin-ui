c006ddaee88628f69ad66bd4b22924f72eca28c3
syafiqa23 <111202314920@mhs.dinus.ac.id>
2025-12-16 11:16:31 +0700

    State in Component with Hooks

---
+++ src/components/Fragments/CardStatistic.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Card from "../Elements/Card";
+
+function CardStatistic() {
+    return (
+        <>
+            <Card
+                title="Recent Transactions"
+                link="/transactions"
+                desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, autem porro asperiores numquam sed veritatis debitis beatae amet laboriosam fuga pariatur sapiente suscipit culpa facere voluptatem. Repellat asperiores doloribus earum!"
+            />
+        </>
+    )
+}
+
+export default CardStatistic
