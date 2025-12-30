171d98c1fb11fa9498ed041c07c0c792a5baf932
syafiqa23 <111202314920@mhs.dinus.ac.id>
2025-12-30 17:54:37 +0700

    Frontend-Backend Integration

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 import MainLayout from '../components/Layouts/MainLayout'
 import Card from "../components/Elements/Card";
 import CardBalance from "../components/Fragments/CardBalance";
@@ -8,31 +8,54 @@ import CardRecentTransaction from "../components/Fragments/CardRecentTransaction
 import CardStatistic from "../components/Fragments/CardStatistic";
 import CardExpenseBreakdown from "../components/Fragments/CardExpenseBreakdown";
 import { transactions, bills, expensesBreakdowns, balances, goals, expensesStatistics, } from "../data";
+import { goalService } from "../services/dataService";
+import { AuthContext } from "../context/authContext";
 
-function dashboard() {
+function Dashboard() {
+  const [goals, setGoals] = useState({});
+  const { logout } = useContext(AuthContext);
+
+  const fetchGoals = async () => {
+    try {
+      const data = await goalService();
+      setGoals(data);
+    } catch (err) {
+      console.error("Gagal mengambil data goals:", err);
+      if (err.status === 401) {
+        logout();
+      }
+    }
+  };
+
+  useEffect(() => {
+    fetchGoals();
+  }, []);
+
+  console.log(goals);
+  
   return (
     <>
       <MainLayout>
         <div className="grid sm:grid-cols-12 gap-6">
           <div className="sm:col-span-4">
-            <CardBalance data={balances}/>
+            <CardBalance data={balances} />
           </div>
           <div className="sm:col-span-4">
-            <CardGoal data={goals}/>
+            <CardGoal data={goals} />
           </div>
           <div className="sm:col-span-4">
-            <CardUpcomingBill data={bills}/>
+            <CardUpcomingBill data={bills} />
           </div>
           <div className="sm:col-span-4 sm:row-span-2">
-            <CardRecentTransaction data={transactions}/>
+            <CardRecentTransaction data={transactions} />
 
           </div>
           <div className="sm:col-span-8">
-            <CardStatistic data={expensesStatistics}/>
+            <CardStatistic data={expensesStatistics} />
 
           </div>
           <div className="sm:col-span-8">
-            <CardExpenseBreakdown data={expensesBreakdowns}/>
+            <CardExpenseBreakdown data={expensesBreakdowns} />
 
           </div>
         </div>
@@ -41,4 +64,4 @@ function dashboard() {
   )
 }
 
-export default dashboard
+export default Dashboard
